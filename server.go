39abbf4b3c9a1f19556e2464f2d317e7cc2529a6
Michal Guerquin <michalg@allenai.org>
2017-06-08 14:51:21 -0700

    tests

--- server.go
+++ server.go
@@ -9,25 +9,27 @@ import (
 
 var happy bool = true
 
-var hostname string = os.Getenv("HOSTNAME")
-var serverAddr string = os.Getenv("SERVER_ADDR")
+func hostname() string {
+	return os.Getenv("HOSTNAME")
+}
 
 func serverDescription() string {
 	if happy {
-		return "Happy server on host " + hostname
+		return "Happy server on host " + hostname()
 	} else {
-		return "Sad server on host " + hostname
+		return "Sad server on host " + hostname()
 	}
 }
 
-func getsad(w http.ResponseWriter, r *http.Request) {
+func MakeSad(w http.ResponseWriter, r *http.Request) {
 	response := serverDescription() + " is now sad\n"
 	happy = false
 	fmt.Println(response)
 	io.WriteString(w, response)
 	return
 }
-func gethappy(w http.ResponseWriter, r *http.Request) {
+
+func MakeHappy(w http.ResponseWriter, r *http.Request) {
 	response := serverDescription() + " is now happy\n"
 	happy = true
 	fmt.Println(response)
@@ -35,7 +37,7 @@ func gethappy(w http.ResponseWriter, r *http.Request) {
 	return
 }
 
-func something(w http.ResponseWriter, r *http.Request) {
+func Something(w http.ResponseWriter, r *http.Request) {
 	if !happy {
 		w.WriteHeader(http.StatusInternalServerError)
 	}
@@ -46,9 +48,11 @@ func something(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
-	http.HandleFunc("/something", something)
-	http.HandleFunc("/sad", getsad)
-	http.HandleFunc("/happy", gethappy)
+	http.HandleFunc("/something", Something)
+	http.HandleFunc("/sad", MakeSad)
+	http.HandleFunc("/happy", MakeHappy)
+
+	serverAddr := os.Getenv("SERVER_ADDR")
 	fmt.Printf("Starting server on address %s\n", serverAddr)
 	err := http.ListenAndServe(serverAddr, nil)
 	if err != nil {
---
+++ server_test.go
@@ -0,0 +1,67 @@
+package main
+
+import "testing"
+import "os"
+import "net/http"
+import "net/http/httptest"
+
+func TestServerDescriptionHappy(t *testing.T) {
+	os.Setenv("HOSTNAME", "abc123")
+	happy = true
+
+	if serverDescription() != "Happy server on host abc123" {
+		t.Fatal("Test failed, yo")
+	}
+}
+
+func TestServerDescriptionSad(t *testing.T) {
+	os.Setenv("HOSTNAME", "abc123")
+	happy = false
+
+	if serverDescription() != "Sad server on host abc123" {
+		t.Fatal("Test failed, yo")
+	}
+}
+
+func confirm(t *testing.T, handler http.HandlerFunc, path string, expectedStatus int, expectedBody string) {
+	// Create a request to pass to our handler. We don't have any query parameters for now, so we'll
+	// pass 'nil' as the third parameter.
+	req, err := http.NewRequest("GET", path, nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// We create a ResponseRecorder (which satisfies http.ResponseWriter) to record the response.
+	rr := httptest.NewRecorder()
+
+	// Our handlers satisfy http.Handler, so we can call their ServeHTTP method
+	// directly and pass in our Request and ResponseRecorder.
+	handler.ServeHTTP(rr, req)
+
+	// Check the status code is what we expect.
+	if status := rr.Code; status != expectedStatus {
+		t.Errorf("handler returned wrong status code: got %v want %v",
+			status, expectedStatus)
+	}
+
+	// Check the response body is what we expect.
+	if rr.Body.String() != expectedBody {
+		t.Errorf("handler returned unexpected body: got %v want %v",
+			rr.Body.String(), expectedBody)
+	}
+
+}
+
+func TestHappy(t *testing.T) {
+	os.Setenv("HOSTNAME", "abc123")
+	happy = false
+	confirm(t, MakeHappy, "/something", 200, "Sad server on host abc123 is now happy\n")
+	confirm(t, Something, "/something", 200, "Happy server on host abc123 handling request: /something\n")
+}
+
+func TestSad(t *testing.T) {
+	os.Setenv("HOSTNAME", "abc123")
+	happy = true
+	confirm(t, MakeSad, "/something", 200, "Happy server on host abc123 is now sad\n")
+	confirm(t, Something, "/something", 500, "Sad server on host abc123 handling request: /something\n")
+}
